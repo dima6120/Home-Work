6776d54e742a4a8e2ad0434dc3b2804ba125fc5a
dima6120 <dima6120@gmail.com>
2012-09-20 22:35:06 +0400

    исправлен Palindrome, проблема была в Scanner

--- Palindrome/src/palindrome/Main.java
+++ Palindrome/src/palindrome/Main.java
@@ -5,18 +5,17 @@
 
 package palindrome;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        Scanner sc = new Scanner(System.in);
-        String s = ""; Palindrome p;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s; Palindrome p;
         p = new CaseInSensetivePalindrome();
-        if (sc.hasNext())
-        {
-            s = sc.next();
-        }
+        s = br.readLine();
         System.out.println(p.isPalindrome(s));
     }
 }
