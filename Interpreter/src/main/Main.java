a699373319a7e786a1f9a701285c054d970f3ae4
dima6120 <dima6120@gmail.com>
2012-11-01 01:55:57 +0300

    Interpreter: добавлен parser1

--- Interpreter/src/main/Main.java
+++ Interpreter/src/main/Main.java
@@ -8,14 +8,14 @@ package main;
 
 import interpreter.*;
 import interpreter.exceptions.*;
-import interpreter.parser.Parser;
+import interpreter.parser.*;
 import interpreter.treenodes.Expression;
 import interpreter.treenodes.Node;
 
 public class Main {
     public static void main(String[] args) throws LexemeTypeMismatchException, UnexectedLexemException, UnexectedSymbolException, DivByZeroException, TypeMismatchException, UnexpectedTypeException {
-        Parser pr = new Parser();
-        Node n = pr.parse("(fun x -> fun y -> y x) 1 (fun x -> x + 1)");
+        Parser1 pr = new Parser1();
+        Node n = pr.parse("let f = fun x -> x + 1 in let x = 2 in f x");
         // ((fun x -> x) (fun x -> x + 5)) 3
         // let f = fun x -> fun y -> x + y in f ((f 1) 1) 1
         Interpreter i = new LazyInterpreter();
