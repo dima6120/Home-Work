bcd675bd3d3879aa238cecec0afb014887377112
dima6120 <dima6120@gmail.com>
2012-11-04 22:50:25 +0300

    Palindrome: добавлена ф-я реализующая отличающийся код у InS и S Palindrome

--- Palindrome/src/palindrome/CaseInSensitivePalindrome.java
+++ Palindrome/src/palindrome/CaseInSensitivePalindrome.java
@@ -7,41 +7,9 @@
 package palindrome;
 
 public class CaseInSensitivePalindrome extends Palindrome {
+
     @Override
-    public boolean isPalindrome(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException("передана не инициализированная "
-                    + "строка!");
-        }
-        
-        int left = 0, right = s.length() - 1;
-        
-        while (left < right) {
-               while (!isletordig(s.charAt(left))){
-                       if (left == right) {
-                           return true;
-                       }
-                       left++;
-               }
-
-               while (!isletordig(s.charAt(right))){
-                       if (left == right) {
-                           return true;
-                       }
-                       right--;
-               }
-
-               if (Character.toLowerCase(s.charAt(left)) 
-                       != Character.toLowerCase(s.charAt(right))){
-                       return false;
-               }
-
-               left++;
-
-               right--;
-        }
-        
-        return true;
+    boolean notequal(char c1, char c2) {
+        return Character.toLowerCase(c1) != Character.toLowerCase(c2);
     }
-    
 }
--- Palindrome/src/palindrome/Palindrome.java
+++ Palindrome/src/palindrome/Palindrome.java
@@ -15,5 +15,39 @@ public abstract class Palindrome {
         }
         return false;
     }
-    public abstract boolean isPalindrome(String s);
+    abstract boolean notequal(char c1, char c2);
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("передана не инициализированная "
+                    + "строка!");
+        }
+        
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+               while (!isletordig(s.charAt(left))){
+                       if (left == right) {
+                           return true;
+                       }
+                       left++;
+               }
+
+               while (!isletordig(s.charAt(right))){
+                       if (left == right) {
+                           return true;
+                       }
+                       right--;
+               }
+
+               if (notequal(s.charAt(left), s.charAt(right))) {
+                   return false;
+               }
+
+               left++;
+
+               right--;
+        }
+        
+        return true;
+    }
 }
